700bb5d333d7cba2a060e487c59e9d35cb38451a
Leonardo Situmorang <leonardo@situmorang.net>
2013-01-13 03:49:36 +0800

    add physics to rocket it's now able to fall, and so does fly (by pressing, or press then holding space bar)

--- rocket.lua
+++ rocket.lua
@@ -1,40 +1,54 @@
+require "world"
+
 rocket = {}
 
-local g = love.graphics
+local g, p = love.graphics, love.physics
 
 function rocket.load()
   rocket.widthScale = 1
   rocket.image = rocket.image or g.newImage("assets/rocket_small.png")
   rocket.width = rocket.image:getWidth() * rocket.widthScale
+  rocket.height = rocket.image:getHeight()
   rocket.speed = 100;
   rocket.x = (g.getWidth() - rocket.width) / 2
   rocket.y = 100
 
   --print('rocket width: '..rocket.image:getWidth()..' rocket height: '..rocket.image:getHeight())
-  print('rocket width: '..rocket.width)
-  print('rocket.x: '..rocket.x)
-  print('window width: '..g.getWidth())
+  --print('rocket width: '..rocket.width)
+  --print('rocket.x: '..rocket.x)
+  --print('window width: '..g.getWidth())
+
+  -- ground: body, shape, fixture(body, shape)
+  -- body: shape will anchor at body's center
+  -- shape: define width and height
+  -- fixture: attach shape to body
+  world.objects.rocket = {}
+  world.objects.rocket.body = p.newBody(world.world, g.getWidth() / 2, rocket.y + (rocket.image:getHeight() / 2), "dynamic")
+  world.objects.rocket.shape = p.newRectangleShape(rocket.image:getWidth(), rocket.image:getHeight())
+  world.objects.rocket.fixture = p.newFixture(world.objects.rocket.body, world.objects.rocket.shape)
 end
 
 function rocket.draw()
   love.graphics.setBackgroundColor(255,255,255)
-  g.draw(rocket.image, rocket.x, rocket.y, 0, rocket.widthScale)
+  g.draw(rocket.image, world.objects.rocket.body:getX() - (rocket.width / 2), world.objects.rocket.body:getY() - (rocket.height / 2), 0, rocket.widthScale)
+  
+  -- dummy object to get visual of rocket's body:
+  -- g.polygon("fill", world.objects.rocket.body:getWorldPoints(world.objects.rocket.shape:getPoints()))
 end
 
 function rocket.update(dt)
-  if love.keyboard.isDown("left") then
-    rocket.x = rocket.x - rocket.speed * dt
+  if love.keyboard.isDown(" ") then
+    -- apply incremental negative impulse from the bottom side
+    -- (am not really sure if my interpretation is correct
+    -- but, the outcome says so. i come to these numbers after
+    -- several test)
+		world.objects.rocket.body:applyLinearImpulse(0, -(10 + (dt * 100)))
   end
+end
 
-  if love.keyboard.isDown("right") then
-    rocket.x = rocket.x + rocket.speed * dt
-  end
-  
-  if love.keyboard.isDown("up") then
-    rocket.y = rocket.y - rocket.speed * dt
-  end
-
-  if love.keyboard.isDown("down") then
-    rocket.y = rocket.y + rocket.speed * dt
-  end
+function rocket.keypressed(k)
+	if k == " " then
+		-- Apply a random impulse
+		world.objects.rocket.body:applyLinearImpulse(0, -150)
+	end
 end
